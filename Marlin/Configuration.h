4e6edb87040a37a52aa2668d8eb303d2bd2cb6c2
Olemis Lang <olemis@simelo.tech>
2020-01-17 21:20:13 -0500

    STRING_CONFIG_H_AUTHOR => CubanTech

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -29,7 +29,7 @@
  * - Type of electronics
  * - Type of temperature sensor
  * - Printer geometry
- * - configuration
+ * - Endstop configuration
  * - LCD controller
  * - Extra features
  *
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Marcos Maceo, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(CubanTech, default config)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
